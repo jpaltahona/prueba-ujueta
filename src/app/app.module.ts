e4f07d4c38b6be9d73151d90ca3812ba50df2d63
jpaltahona <jpaltahonacc@gmail.com>
2021-09-15 08:22:06 -0500

    settings router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,11 @@ import { InfoComponent } from './info/info.component';
 import { VeterinarianSectionComponent } from './veterinarian-section/veterinarian-section.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +21,15 @@ import { FooterComponent } from './footer/footer.component';
     BannerComponent,
     InfoComponent,
     VeterinarianSectionComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
